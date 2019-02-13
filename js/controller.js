de8cf77797d6d6bc125aaa7b297a82f1b12eb142
Devesh Kumar <devsoft@gmail.com>
2019-02-12 22:38:04 -0800

    Add files via upload

---
+++ js/controller.js
@@ -0,0 +1,11 @@
+function MainCtrl($scope) {
+  $scope.items = [
+    {
+      "name" : "Stress ball",
+      "quantity" : "1",
+      "price" : "1.00 $",
+      "img" : "img/sphero.jpg",
+      "description" : "Awesome robotic ball that can be remote-controlled!"
+    }
+  ];
+}
